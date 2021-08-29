f5e50596ee63dc5edd09e67f195eb9f4d553f495
muntahaasghar <muntahaasghar09@gmail.com>
2021-08-29 09:53:23 -0400

    revStr

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is Muntaha 's project");
-	// write your code here
+        System.out.println("This is Beknazar's project");
+        System.out.println(revStr("Hello World"));
+    }
+
+    public static String revStr(String str) {
+        return new StringBuilder(str).reverse().toString();
     }
 }
